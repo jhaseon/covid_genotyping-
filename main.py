c2904f9a398bdb4a50b04addb8a4784b4f93a613
Hank Yun <jhaseon@gmail.com>
2020-09-07 11:05:51 -0400

    new model and small edits

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.title('COVID-19 Genotyping App')
 
 st.write('Hank Yun: NYCDSA Capstone Project')
 
-model = keras.models.load_model(os.path.join(PROJECT_PATH, 'model'))
+model = keras.models.load_model("/home/jhaseon/projects/covid/model2")
 
 img_width, img_height = 30, 120
 
@@ -26,6 +26,7 @@ uploaded_file = st.file_uploader("Choose a JPG file", type="jpg")
 if uploaded_file is not None:
     ## preprocessing
     img = Image.open(io.BytesIO(uploaded_file.read()))
+    img = img.convert('RGB')
     img = img.resize((img_width, img_height))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
